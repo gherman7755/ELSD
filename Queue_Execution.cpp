f7c83b7ce280e9027a9015f4a6fe533e96041f26
gherman7755 <58359494+gherman7755@users.noreply.github.com>
2021-02-20 10:20:20 +0300

    Update Queue_Execution.cpp

--- Queue_Execution.cpp
+++ Queue_Execution.cpp
@@ -6,52 +6,88 @@ using namespace std;
 
 queue<int> q;
 
-string command_empty = "empty", command_pop = "pop";
-string command_front = "front", command_back = "back";
-string command_push = "push";
+void queue_edit(string input){
+
+    string();
+    char temp[200] = "";
 
 
-
-void data_structure(string input){
-
-    string current_command;
-    string value;
-
-    for(char i : input){
-        if(i != '('){
-            current_command += i;
+    if(input[0] == 'f'){
+        string command = "front()";
+        for(int i = 0; i < input.size(); i++){
+            temp[i] = input[i];
         }
-        else{break;}
-    }
 
-
-    if(current_command == command_push){
-        for(char i : input.substr(5)){
-            if(i != ')')
-                value += i;
+        if(temp == command) {
+            cout << q.front() << endl;
+            return;
         }
-        q.push(stoi(value));
-        return;
     }
 
-    else if(current_command == command_pop){
-        q.pop();
-        return;
+
+    else if(input[0] == 'b'){
+        string command = "back()";
+        for(int i = 0; i < input.size(); i++){
+            temp[i] = input[i];
+        }
+
+        if(temp == command) {
+            cout << q.back() << endl;
+            return;
+        }
     }
 
-    else if(current_command == command_front){
-        cout << q.front() << endl;
-        return;
+    else if(input[0] == 'e'){
+        string command = "empty()";
+        for(int i = 0; i < input.size(); i++){
+            temp[i] = input[i];
+        }
+
+        if(temp == command) {
+            cout << q.empty() << endl;
+            return;
+        }
     }
 
-    else if(current_command == command_back){
-        cout << q.back() << endl;
-        return;
+    else if(input[0] == 'p'){
+        bool is_command = false;
+        string command = "push";
+        for(int i = 0; i < input.size(); ++i){
+            while(input[i] != '('){
+                temp[i] = input[i];
+                ++i;
+            }
+            break;
+        }
+
+        if(temp == command)
+            is_command = true;
+
+        command = "pop()";
+        for(int i = 0; i < input.size(); i++){
+            temp[i] = input[i];
+        }
+        if(temp == command){
+            q.pop();
+            return;
+        }
+
+        char value[200] = "";
+        if(is_command){
+            for(int i = 0, j = 0; i < input.size(); i++){
+                if(input[i] == '('){
+                    i++;
+                    while(input[i] != ')'){
+                        value[j] = input[i];
+                        i++;
+                        j++;
+                    }
+                }
+            }
+            q.push(stoi(value));
+            return;
+        }
     }
 
-    else if(current_command == command_empty){
-        cout << q.empty() << endl;
-        return;
-    }
 
 }
