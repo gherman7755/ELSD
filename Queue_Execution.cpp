6c54fa4a70d159f4f18c4f8c7c46c4fbdd1d098f
gherman7755 <58359494+gherman7755@users.noreply.github.com>
2021-02-26 22:25:43 +0300

    Update Queue_Execution.cpp

--- Queue_Execution.cpp
+++ Queue_Execution.cpp
@@ -1,6 +1,7 @@
 #include "Queue_Execution.h"
 #include <queue>
 #include <string>
+#include "VerythingCommand.h"
 
 using namespace std;
 
@@ -12,67 +13,122 @@ void queue_edit(string input){
 
 
     if(input[0] == 'f'){
-        string command = "front()";
-        for(int i = 0; i < input.size(); i++){
-            temp[i] = input[i];
-        }
-
-        if(temp == command) {
-            cout << q.front() << endl;
-            return;
+        if(check_command(input)) {
+            string command = "front";
+            for (int i = 0; i < input.size(); i++) {
+                if (input[i] == '(')
+                    break;
+                else
+                    temp[i] = input[i];
+            }
+            if (temp == command) {
+                cout << q.front() << endl;
+                return;
+            }
+            else{
+                cout << "Invalid command" << endl;
+                exit(0);
+            }
         }
     }
 
 
     else if(input[0] == 'b'){
-        string command = "back()";
-        for(int i = 0; i < input.size(); i++){
-            temp[i] = input[i];
-        }
-
-        if(temp == command) {
-            cout << q.back() << endl;
-            return;
+        if(check_command(input)) {
+            string command = "back";
+            for (int i = 0; i < input.size(); i++) {
+                if (input[i] == '(')
+                    break;
+                else
+                    temp[i] = input[i];
+            }
+            if (temp == command) {
+                cout << q.back() << endl;
+                return;
+            }
+            else{
+                cout << "Invalid command" << endl;
+                exit(0);
+            }
         }
     }
 
     else if(input[0] == 'e'){
-        string command = "empty()";
-        for(int i = 0; i < input.size(); i++){
-            temp[i] = input[i];
+        if(check_command(input)) {
+            string command = "empty";
+            for (int i = 0; i < input.size(); i++) {
+                if (input[i] == '(')
+                    break;
+                else
+                    temp[i] = input[i];
+            }
+            if (temp == command) {
+                cout << q.empty() << endl;
+                return;
+            }
+            else{
+                cout << "Invalid command" << endl;
+                exit(0);
+            }
         }
+    }
 
-        if(temp == command) {
-            cout << q.empty() << endl;
-            return;
+    else if(input[0] == 'd'){
+        if(check_command(input)) {
+            string command = "draw";
+            for (int i = 0; i < input.size(); i++) {
+                if (input[i] == '(')
+                    break;
+                else
+                    temp[i] = input[i];
+            }
+            if (temp == command) {
+                exit(0);
+            }
+            else{
+                cout << "Invalid command" << endl;
+                exit(0);
+            }
         }
     }
 
     else if(input[0] == 'p'){
-        bool is_command = false;
-        string command = "push";
-        for(int i = 0; i < input.size(); ++i){
-            while(input[i] != '('){
-                temp[i] = input[i];
-                ++i;
+            bool is_admit = false;
+            bool is_command = false;
+            string command = "push";
+            for(int i = 0; i < input.size(); ++i){
+                if(input[i] == '('){
+                    break;
+                }
+                else
+                    temp[i] = input[i];
             }
-            break;
-        }
 
-        if(temp == command)
-            is_command = true;
+            if(temp == command) {
+                is_command = true;
+                is_admit = is_push(input);
+            }
 
-        command = "pop()";
-        for(int i = 0; i < input.size(); i++){
-            temp[i] = input[i];
-        }
-        if(temp == command){
-            q.pop();
-            return;
-        }
+
+            command = "pop";
+            for (int i = 0; i < input.size(); i++) {
+                if (input[i] == '(')
+                    break;
+                else
+                    temp[i] = input[i];
+                }
+            if (temp == command) {
+                if(check_command(input)){
+                    q.pop();
+                    return;
+                }
+            }else if(not is_command){
+                    cout << "Invalid command" << endl;
+                    exit(0);
+                }
 
         char value[200] = "";
-        if(is_command){
+        if(is_command and is_admit){
             for(int i = 0, j = 0; i < input.size(); i++){
                 if(input[i] == '('){
                     i++;
@@ -86,7 +142,10 @@ void queue_edit(string input){
             q.push(stoi(value));
             return;
         }
+
     }
-
-
+    else{
+        cout << "Invalid command" << endl;
+        exit(0);
+    }
 }
