9a8ff35d8db70e5164c99f56b23ea3de06292a0d
gherman7755 <58359494+gherman7755@users.noreply.github.com>
2021-03-01 17:29:03 +0300

    Update Queue_Execution.cpp

--- Queue_Execution.cpp
+++ Queue_Execution.cpp
@@ -9,143 +9,31 @@ queue<int> q;
 
 void queue_edit(string input){
 
-    char temp[200] = "";
+    int value = 0;
 
+    int choose = analyze_input(input, &value);
 
-    if(input[0] == 'f'){
-        if(check_command(input)) {
-            string command = "front";
-            for (int i = 0; i < input.size(); i++) {
-                if (input[i] == '(')
-                    break;
-                else
-                    temp[i] = input[i];
-            }
-            if (temp == command) {
-                cout << q.front() << endl;
-                return;
-            }
-            else{
-                cout << "Invalid command" << endl;
-                exit(0);
-            }
-        }
-    }
-
-
-    else if(input[0] == 'b'){
-        if(check_command(input)) {
-            string command = "back";
-            for (int i = 0; i < input.size(); i++) {
-                if (input[i] == '(')
-                    break;
-                else
-                    temp[i] = input[i];
-            }
-            if (temp == command) {
-                cout << q.back() << endl;
-                return;
-            }
-            else{
-                cout << "Invalid command" << endl;
-                exit(0);
-            }
-        }
-    }
-
-    else if(input[0] == 'e'){
-        if(check_command(input)) {
-            string command = "empty";
-            for (int i = 0; i < input.size(); i++) {
-                if (input[i] == '(')
-                    break;
-                else
-                    temp[i] = input[i];
-            }
-            if (temp == command) {
-                cout << q.empty() << endl;
-                return;
-            }
-            else{
-                cout << "Invalid command" << endl;
-                exit(0);
-            }
-        }
-    }
-
-    else if(input[0] == 'd'){
-        if(check_command(input)) {
-            string command = "draw";
-            for (int i = 0; i < input.size(); i++) {
-                if (input[i] == '(')
-                    break;
-                else
-                    temp[i] = input[i];
-            }
-            if (temp == command) {
-                exit(0);
-            }
-            else{
-                cout << "Invalid command" << endl;
-                exit(0);
-            }
-        }
-    }
-
-    else if(input[0] == 'p'){
-            bool is_admit = false;
-            bool is_command = false;
-            string command = "push";
-            for(int i = 0; i < input.size(); ++i){
-                if(input[i] == '('){
-                    break;
-                }
-                else
-                    temp[i] = input[i];
-            }
-
-            if(temp == command) {
-                is_command = true;
-                is_admit = is_push(input);
-            }
-
-
-            command = "pop";
-            for (int i = 0; i < input.size(); i++) {
-                if (input[i] == '(')
-                    break;
-                else
-                    temp[i] = input[i];
-                }
-            if (temp == command) {
-                if(check_command(input)){
-                    q.pop();
-                    return;
-                }
-            }else if(not is_command){
-                    cout << "Invalid command" << endl;
-                    exit(0);
-                }
-
-        char value[200] = "";
-        if(is_command and is_admit){
-            for(int i = 0, j = 0; i < input.size(); i++){
-                if(input[i] == '('){
-                    i++;
-                    while(input[i] != ')'){
-                        value[j] = input[i];
-                        i++;
-                        j++;
-                    }
-                }
-            }
-            q.push(stoi(value));
-            return;
-        }
-
-    }
-    else{
-        cout << "Invalid command" << endl;
-        exit(0);
+    switch (choose) {
+        case 1:
+            cout << q.front() << endl;
+            break;
+        case 2:
+            cout << q.back() << endl;
+            break;
+        case 3:
+            cout << q.empty() << endl;
+            break;
+        case 4:
+            cout << "Drawing" << endl;
+            exit(0);
+        case 5:
+            q.pop();
+            break;
+        case 6:
+            q.push(value);
+            break;
+        default:
+            cout << "It is impossible" << endl;
+            exit(0);
     }
 }
