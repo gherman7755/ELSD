14d51b6f87f0a00f5c00b3606eb6a07aa5dc4d17
gherman7755 <58359494+gherman7755@users.noreply.github.com>
2021-03-31 18:17:32 +0300

    Update enteryDictionary.cpp

--- enteryDictionary.cpp
+++ enteryDictionary.cpp
@@ -3,79 +3,85 @@
 #include "enteryDictionary.h"
 using namespace std;
 
+bool is_parameter(string input){
+    string parameter = "queue";
+    char par[100] = "";
+    for (int i = input.find('_') + 1, j = 0; i <= input.size(); ++i) {
+        if (input[i] == '(')
+            break;
+
+        else {
+            par[j] = input[i];
+            j++;
+        }
+    }
+    if (par == parameter)
+        return true;
+    else{
+        cout << "Invalid parameter" << endl;
+        exit(0);
+    }
+}
+
+bool is_create(string input){
+    string create_command = "create";
+    char temp[100] = "";
+    for (int i = 0; i <= input.size(); i++) {
+        if (input[i] != '_')
+            temp[i] = input[i];
+        else
+            break;
+    }
+
+    if (temp == create_command){
+        if(is_parameter(input))
+            return true;
+    }
+    else{
+        cout << "Invalid command" << endl;
+        exit(0);
+    }
+}
+
+bool are_empty_brackets(string input){
+    if (input.find('(') + 1 == input.find(')')){
+        if(is_create(input))
+            return true;
+        }
+    else{
+        cout << "Unexpected value in brackets" << endl;
+        exit(0);
+    }
+}
+
+bool check_brackets(string input){
+    if (input.find('(') < input.size() and input.find(')') < input.size()){
+        if(are_empty_brackets(input))
+            return true;
+    }
+    else{
+        cout << "Expected brackets" << endl;
+        exit(0);
+    }
+}
+
+bool valid_semicolon(string input){
+    char end = ';';
+    if(input.find(end) == input.size() - 1){
+        if(check_brackets(input))
+            return true;
+    }
+    else{
+        cout << "Expected \';\' or incorrect position" << endl;
+        exit(0);
+    }
+}
+
 bool valid_start(string input){
     char s = '_';
     if(input.find(s) < 20) {
-        bool is_end = false;
-        bool are_brackets = false;
-        bool is_parameter = false;
-        bool is_create_command = false;
-        bool are_empty_brackets = false;
-        string create_command = "create";
-        string queue_parameter = "queue";
-
-
-        if (input.size() - 1 == input.find(';'))
-            is_end = true;
-
-        if (input.find('(') < input.size() and input.find(')') < input.size())
-            are_brackets = true;
-
-        if (are_brackets)
-            if (input.find('(') + 1 == input.find(')'))
-                are_empty_brackets = true;
-
-        char temp[100] = "";
-        for (int i = 0; i <= input.size(); i++) {
-            if (input[i] != '_')
-                temp[i] = input[i];
-            else
-                break;
-        }
-
-        if (temp == create_command)
-            is_create_command = true;
-
-        char par[100] = "";
-        for (int i = input.find('_') + 1, j = 0; i <= input.size(); ++i) {
-            if (input[i] == '(')
-                break;
-
-            else {
-                par[j] = input[i];
-                j++;
-            }
-        }
-        if (par == queue_parameter)
-            is_parameter = true;
-
-        if (is_end) {
-            if (are_brackets) {
-                if (are_empty_brackets) {
-                    if (is_create_command) {
-                        if (is_parameter)
-                            return true;
-                        else{
-                            cout << "Invalid parameter" << endl;
-                            exit(0);
-                        }
-                    } else{
-                        cout << "Invalid command" << endl;
-                        exit(0);
-                    }
-                } else {
-                    cout << "Unexpected value in brackets" << endl;
-                    exit(0);
-                }
-            }
-            else{
-                cout << "Expected brackets" << endl;
-                exit(0);
-            }
-        }
-        else {
-            cout << "Expected \';\'" << endl;
-            exit(0);
+        if(valid_semicolon(input)){
+            return true;
         }
     }
     else{
